6a28be94d645bc2a077aaf1d819d41593e5acd2c
Yassine Dahmoul <Yassine.Dahmoul@grenoble-inp.org>
2018-11-08 15:29:07 +0100

    mapping du fichier .in

--- src/AODjustify.c
+++ src/AODjustify.c
@@ -1,16 +1,16 @@
 /**
  * \file AODjustify.c
  * \brief Programme de justification optimale d'un texte (cf TP AOD 2018-2019).
- * \author 
+ * \author
  * \version 0.1
  * \date octobre 2018
  *
- * Usage AODjustify <M> <file> 
- * copie le fichier <file>.in dans le fichier <file>.out  en le justifiant optimalement sur une ligne de taille <M>. 
+ * Usage AODjustify <M> <file>
+ * copie le fichier <file>.in dans le fichier <file>.out  en le justifiant optimalement sur une ligne de taille <M>.
  */
 
 #include <stdio.h>
-#include <string.h> // strlen, 
+#include <string.h> // strlen,
 #include <sys/mman.h> // pour mmap
 #include <sys/stat.h>
 #include <math.h>
@@ -23,15 +23,16 @@
 #include <ctype.h> // isspace
 
 
+
 /**
  * \fn usage(const char * error_msg)
  * \brief Fonction d'affichage d'un message d'érreur et de la bonne utilisation du programme. Arrêt de l'exécution (exit(-1)).
  *
- * \param error_msg message d'erreur à afficher 
- * \return void 
+ * \param error_msg message d'erreur à afficher
+ * \return void
  */
 void usage(char * error_msg) {
-   fprintf( stderr, "AODjustify ERROR> %s\n", error_msg) ; 
+   fprintf( stderr, "AODjustify ERROR> %s\n", error_msg) ;
    fprintf( stderr, "AODjustify ERROR> Usage: AODjustify <M> <file> \n"
                     "  copie le fichier <file>.in dans le fichier <file>.out  en le justifiant optimalement sur une ligne de taille <M>. \n") ;
    exit(-1) ;
@@ -84,11 +85,31 @@ int Bellman(void* file, int i, int M, int* bellman_mem){
     return bellman_mem[i];
 }
 
-
+size_t getFilesize(const char* filename) {
+    struct stat st;
+    stat(filename, &st);
+    return st.st_size;
+}
 
 int main(int argc, char** argv) {
-  if (argc != 3) usage("Mauvais nombre de paramètres dans l'appel.") ;
+    if (argc != 3) usage("Mauvais nombre de paramètres dans l'appel.") ;
+    int M = argv[1];
+    size_t filesize = getFilesize(argv[2]);
+    //On ouvre le fichier et recupere sa reference
+    int fd = open(argv[1], O_RDONLY, 0);
+    assert(fd != -1);
+    //On execute mmap
+    void* mmappedData = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd, 0);
+    assert(mmappedData != MAP_FAILED);
 
-  // A vous de compléter
+
+
+
+    //Cleanup
+    int rc = munmap(mmappedData, filesize);
+    assert(rc == 0);
+    close(fd);
+
+
   return 0 ;
 }
