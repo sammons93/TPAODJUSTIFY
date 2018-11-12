d80c8544d3c3bd193666d97fa6afbe516d06b78e
sammons93 <monsinjs@SAM-PC.localdomain>
2018-11-12 01:42:28 +0100

    impossible à finir

--- src/AODjustify.c
+++ src/AODjustify.c
@@ -21,6 +21,7 @@
 #include <fcntl.h> // open
 #include <assert.h> // assert
 #include <ctype.h> // isspace
+#include <stdbool.h>
 #include "AODjustify.h"
 
 
@@ -38,31 +39,210 @@ void usage(char *error_msg) {
     exit(-1);
 }
 
+/**
+ * Tableaux de memoisation
+ * tab_mem stocke les valeurs Bellman(i) pour chaque paragraphe du fichier
+ * E_glob stocke tous les E(i,k) pour chaque paragraphe du fichier
+ */
+static cell_bell **tab_mem;
+static long long ***E_glob;
 
-static cell_bell *tab_mem;
-static long *E_glob;
+
+/**
+ *
+ * @param filename
+ * @return taille du fichier filename
+ */
+size_t getFilesize(const char *filename) {
+    struct stat st;
+    stat(filename, &st);
+    return st.st_size;
+}
 
 
-long long Bellman(long long *tailles, int nb_mots, int i, long long M) {
-    if (tab_mem[i].bell != -1) {
-        return tab_mem[i].bell;
+long long puissance(long long a, long long b){
+    long long puissance = a;
+    for (int i = 0; i < b-1 ; ++i) {
+        puissance *= a;
+    }
+    return puissance;
+}
+
+/**
+ * ecrit le nom du fichier.out
+ * @param nom_in
+ * @return
+ */
+char *ecriture_out(const char *nom_in) {
+    size_t taille = strlen(nom_in);
+    size_t taille_copie = taille + 1;
+    char *copie = malloc((taille_copie + 1) * sizeof(char));
+    uint32_t i = 0;
+    while ((nom_in[i] != '.') && (i < taille)) {
+        copie[i] = nom_in[i];
+        i++;
+    }
+    if (i < taille) {
+        char completion[] = ".out";
+        strcat(copie, completion);
+    }
+    return copie;
+}
+
+/**
+ * Recupere le nombre de paragraphes du fichier file
+ * @param file
+ * @param filesize
+ * @return nb_paragraphes
+ */
+int recup_nb_para(char *file, int filesize) {
+    int nb_paragraphes = 0;
+    size_t ind_caractere = 0;
+    while (ind_caractere < filesize - 3) {
+        if ((file[ind_caractere] == 10) && (file[ind_caractere + 1] == 10)) {
+            nb_paragraphes++;
+        }
+        ind_caractere++;
+    }
+    nb_paragraphes++;
+    return nb_paragraphes;
+}
+
+/**
+ * Retourne deux tableaux d'indices de debut et de fin de chaque paragraphe du fichier d'entree
+ * @param mmappedData
+ * @param nb_paragraphes
+ * @param filesize
+ * @return tableaux
+ */
+int **getIndiceLastChar(char *mmappedData, int nb_paragraphes, size_t filesize) {
+    size_t indice_caractere = 0;
+    int indice_paragraphe = 0;
+    int **tableaux = malloc(2 * sizeof(int *));
+    int *tab_indices_fin_paragraphes = malloc(nb_paragraphes * sizeof(int));
+    int *tab_indices_debut_paragraphes = malloc(nb_paragraphes * sizeof(int));
+    while (indice_caractere < filesize - 1) {
+        if ((mmappedData[indice_caractere] == 10) && (mmappedData[indice_caractere + 1] == 10)) {
+            tab_indices_debut_paragraphes[indice_paragraphe + 1] = indice_caractere + 2;
+            if ((mmappedData[indice_caractere - 1]) == 10) {
+                tab_indices_fin_paragraphes[indice_paragraphe] = indice_caractere + 1;
+            } else {
+                tab_indices_fin_paragraphes[indice_paragraphe] = indice_caractere - 1;
+            } //Si ca finit par trois retour chariots
+//            printf("Debut du paragraphe %i: %i\n", indice_paragraphe, tab_indices_debut_paragraphes[indice_paragraphe]);
+//            printf("Fin du paragraphe %i: %i\n", indice_paragraphe, tab_indices_fin_paragraphes[indice_paragraphe]);
+            indice_paragraphe++;
+        }
+        indice_caractere++;
+    }
+    /*cas du dernier paragraphe si il ne finit pas par des retours chariot*/
+    if (!((mmappedData[indice_caractere - 1] == 10) && (mmappedData[indice_caractere] == 10))) {
+        tab_indices_debut_paragraphes[indice_paragraphe + 1] = indice_caractere + 3;
+        tab_indices_fin_paragraphes[indice_paragraphe] = indice_caractere;
+//        printf("Debut du paragraphe %i %i\n", indice_paragraphe, tab_indices_debut_paragraphes[indice_paragraphe]);
+//        printf("Fin du paragraphe %i %i\n", indice_paragraphe, tab_indices_fin_paragraphes[indice_paragraphe]);
+    }
+    tableaux[0] = tab_indices_debut_paragraphes;
+    tableaux[1] = tab_indices_fin_paragraphes;
+    return tableaux;
+}
+
+/**
+ * recupere le nombre de mots du paragraphe commençant par le caractère d'indice debut et finissant par le caractere
+ * d'indice fin
+ * @param file
+ * @param debut
+ * @param fin
+ * @return nb_mots
+ */
+int recup_nb_mots(char *file, int debut, int fin) {
+    int nb_mots = 0;
+    for (int i = debut; i <= fin - 1; ++i) {
+        if (((file[i] == 32) && (file[i + 1] != 32))) {
+            nb_mots++;
+        }
+    }
+    nb_mots++; //on oublie pas le dernier mot du paragraphe.
+    return nb_mots;
+}
+
+/**
+ * Recupere le tableau des tailles des mots du paragraphe
+ * @param file
+ * @param debut
+ * @param fin
+ * @param nb_mots
+ * @return tailles
+ */
+int *recup_tailles_mots(char *file, int debut, int fin, int nb_mots) {
+    int *tailles = malloc(sizeof(int) * nb_mots);
+    int i = debut;
+    int j = debut;
+    int k = 0;
+    while (k < nb_mots - 1) {
+        while (file[j] != 32 || file[j + 1] == 32) {
+            j++;
+        }
+        tailles[k] = j - i;
+        i += tailles[k] + 1;
+        j++;
+        k++;
+    }
+    tailles[k] = fin - i + 1;
+    return tailles;
+}
+
+/**
+ * Recupere un tableau contenant tous les mots du paragraphe (Ex : mots[15] est le 16e mot du paragraphe)
+ * @param file
+ * @param nb_mots
+ * @param tailles
+ * @param debut
+ * @return mots
+ */
+char **recup_mots(char *file, int nb_mots, int *tailles, int debut) {
+    char **mots = malloc(sizeof(char *) * nb_mots);
+    int k = debut;
+    for (int i = 0; i < nb_mots; ++i) {
+        mots[i] = malloc(sizeof(char) * tailles[i]);
+        for (int j = 0; j < tailles[i]; ++j) {
+            mots[i][j] = file[k];
+            k++;
+        }
+        k++;
+    }
+    return mots;
+}
+
+/*On raisonne sur les mots, c'est pourquoi on a besoin des tableaux tailles_mots ainsi que mots pour pouvoir
+ * continuer */
+
+/**
+ * Algorithme recursif de l'equation de Bellman pour le paragraphe numero num_par du fichier. On raisonne sur
+ * les mots et non sur les caractères.
+ * @param num_par
+ * @param nb_mots
+ * @param i
+ * @param M
+ * @return Bellman(i)
+ */
+long long Bellman(int num_par, int nb_mots, int i, long long M) {
+    if (tab_mem[num_par][i].bell != -1) {
+        return tab_mem[num_par][i].bell;
     }
     int temp_ind;
-    long long temp = 0, min = 0, E_temp;
-
-    if (E_glob[i] >= 0) {
-
-        tab_mem[i].bell = 0;
-        tab_mem[i].ind = i;
+    long long temp = 0, min = 0;
+    if (E_glob[num_par][i][nb_mots - 1] >= 0) {
+        tab_mem[num_par][i].bell = 0;
+        tab_mem[num_par][i].ind = i;
     } else {
         temp_ind = i;
-        E_temp = M - tailles[i];
-        for (int k = 0; k < nb_mots; ++k) {
+        for (int k = i; k < nb_mots; ++k) {
 
-            if (E_temp < 0) break;
+            if (E_glob[num_par][i][k] < 0) break;
 
 
-            temp = Bellman(tailles, nb_mots, k + 1, M) + (E_temp) ^ 3;
+            temp = Bellman(num_par, nb_mots, k + 1, M) + puissance(E_glob[num_par][i][k], 3);
 
             if (k == i) min = temp;
 
@@ -70,38 +250,169 @@ long long Bellman(long long *tailles, int nb_mots, int i, long long M) {
                 min = temp;
                 temp_ind = k;
             }
-
-            E_temp = E_temp - tailles[k+1] - 1;
         }
-        tab_mem[i].bell = min;
-        tab_mem[i].ind = temp_ind;
+        tab_mem[num_par][i].bell = min;
+
+        tab_mem[num_par][i].ind = temp_ind;
     }
-    return tab_mem[i].bell;
+    return tab_mem[num_par][i].bell;
 }
 
+/**
+ * Ecrit dans ligne la ligne du paragraphe num_par commençant par le mot d'indice first et finissant par le mot
+ * d'indice last.
+ * @param num_par
+ * @param ligne
+ * @param mots
+ * @param tailles
+ * @param first
+ * @param last
+ * @param M
+ * @param derniere_ligne
+ */
+void ecrire_ligne(int num_par, char *ligne, char **mots, int *tailles, int first, int last, long long M,
+                  bool derniere_ligne) {
+    int i = first;
+    int j = 0; //Indice parcourant les caractères des mots
+    int k = 0; //Indice parcourant la ligne
 
-size_t getFilesize(const char *filename) {
-    struct stat st;
-    stat(filename, &st);
-    return st.st_size;
+    if (derniere_ligne) {
+        while (i < last) {
+            while (j < tailles[i]) {
+                ligne[k] = mots[i][j];
+                j++;
+                k++;
+            }
+            ligne[k] = 32;
+            k++;
+            i++;
+            j = 0;
+        }
+    } else {
+        int nb_espaces_a_rajouter = E_glob[num_par][first][last];
+        int nb_espaces_rajoutes = 0;
+        int q = nb_espaces_a_rajouter / (last - first);
+        while (i < last) {
+            while (j < tailles[i]) {
+                ligne[k] = mots[i][j];
+                j++;
+                k++;
+            }
+            ligne[k] = 32;
+            k++;
+            while (nb_espaces_rajoutes != nb_espaces_a_rajouter) {
+                ligne[k] = 32;
+                k++;
+                nb_espaces_rajoutes++;
+            }
+            j = 0;
+            i++;
+        }
+    }
+
+    for (int m = 0; m < tailles[last]; ++m) {
+        ligne[k] = mots[last][m];
+        k++;
+    }
+    ligne[k] = 10;
 }
 
-char *ecriture_out(const char *nom_in)
-{
-    size_t taille = strlen(nom_in);
-    size_t taille_copie = taille + 1;
-    char *copie = malloc((taille_copie +1)*sizeof(char));
-    uint32_t i = 0;
-    while ((nom_in[i]!= '.') && (i < taille)) {
-        copie[i] = nom_in[i];
-        i++;
+void write_line_in_file(char *mmapOut, long long sizeout, char **lignes, long long nb_lignes, long long M){
+    long long k = 0;
+    long long ind_char = 0;
+    long long ind_ligne = 0;
+    while (k < sizeout){
+        while (ind_ligne < nb_lignes){
+            while (ind_char < M){
+                mmapOut[k] = lignes[ind_ligne][ind_char];
+                k++;
+                ind_char++;
+            }
+            ind_char = 0;
+            ind_ligne++;
+        }
     }
-    if (i < taille) {
-        char completion[] = ".out";
-        strcat(copie,completion);
-    }
-    return copie;
 }
+
+/**
+ * Retourne le nb de lignes justifiées ainsi que le nombre d'espaces à rajouter du paragraphes num_par
+ * et calcule aussi les E(i,k) ainsi que les valeurs de bellman(i) pour chaque mot.
+ * Ce programme permet egalement de stocker les indices de mots de fin de ligne grâce à la structure cell_bell.
+ * @param num_par
+ * @param mots
+ * @param tailles
+ * @param nb_mots
+ * @param M
+ * @return
+ */
+long long *calculs_paragraphes(int num_par, char **mots, int *tailles, int nb_mots, long long M) {
+    long long *res = malloc(3 * sizeof(long long));
+    int first = 0, last = 0;
+    long long bell_0 = 0;
+    long long nb_ligne = 0, nb_espaces = 0;
+    E_glob[num_par] = malloc(sizeof(long long *) * nb_mots);
+    for (int i = 0; i < nb_mots; ++i) {
+        E_glob[num_par][i] = malloc(sizeof(long long) * nb_mots);
+        E_glob[num_par][i][i] = M - (long long) tailles[i];
+        int k = 0;
+        while (k < nb_mots) {
+            if (k < i) {
+                E_glob[num_par][i][k] = 70000;
+            }
+            if (k > i) {
+                long long deltaik = tailles[k];
+                for (int j = i; j < k; ++j) {
+                    deltaik += tailles[j] + 1;
+                }
+                E_glob[num_par][i][k] = M - deltaik;
+            }
+            k++;
+        }
+    }
+    tab_mem[num_par] = malloc(nb_mots * sizeof(cell_bell));
+
+    for (int i = nb_mots - 1; i >= 0; --i) { ;
+        tab_mem[num_par][i].bell = -1;
+        Bellman(num_par, nb_mots, i, M);
+    }
+    while (E_glob[num_par][first][nb_mots - 1] < 0) {
+        last = tab_mem[num_par][first].ind;
+        nb_espaces += E_glob[num_par][first][last];
+        nb_ligne++;
+        first = last + 1;
+    }
+    bell_0 = tab_mem[num_par][0].bell;
+    res[0] = nb_ligne + 1;
+    res[1] = nb_espaces;
+    res[2] = bell_0;
+    return res;
+}
+
+/**
+ * Justifie le paragraphe num_par. Appelle la fonction ecrire_ligne et remplit les lignes du paragraphe.
+ * @param num_par
+ * @param paragraphe
+ * @param mots
+ * @param tailles
+ * @param nb_mots
+ * @param M
+ * @param nb_ligne
+ */
+void justifier_paragraphe(int num_par, paragraphe paragraphe, char **mots, int *tailles, int nb_mots, long long M,
+                          int nb_ligne) {
+    int first = 0, last = 0;
+    int j = 0;
+    while (E_glob[num_par][first][nb_mots - 1] < 0) { //tant qu'on est pas sur la derniere ligne, on ecrit
+        last = tab_mem[num_par][first].ind;
+        ecrire_ligne(num_par, paragraphe.lignes[j], mots, tailles, first, last, M, 0);
+        j++;
+        first = last + 1;
+    }
+    paragraphe.lignes[nb_ligne - 1] = malloc(sizeof(char) * (M - E_glob[num_par][first][nb_mots - 1] + 1));
+    last = nb_mots - 1;
+    ecrire_ligne(num_par, paragraphe.lignes[nb_ligne - 1], mots, tailles, first, last, M, 1);
+}
+
 
 int main(int argc, char **argv) {
     if (argc != 3) usage("Mauvais nombre de paramètres dans l'appel.");
@@ -113,69 +424,81 @@ int main(int argc, char **argv) {
     //On execute mmap
     char *mmappedData = (char *) mmap(NULL, filesize, PROT_READ, MAP_PRIVATE, fd, 0);
     assert(mmappedData != MAP_FAILED);
-    //initialisation des tableaux de memoïsation
 
-//    tab_mem = malloc(filesize * sizeof(cell_bell));
-//    for (int i = 0; i < filesize; ++i) {
-//        tab_mem[i].bell = -1;
-//        tab_mem[i].ind = i;
-//    }
-//    long long cout = Bellman(mmappedData, 0, M);
-//    printf("cout :%lld\n", cout);
-//    printf("cout 1 :%lld\n", tab_mem[45].bell);
-//    printf("ind :%d\n", tab_mem[45].ind);
+    //On crée le paragraphe text:
+    paragraphe *text;
+    int nb_par = recup_nb_para(mmappedData, filesize);
+    text = malloc(sizeof(paragraphe) * nb_par);
+    tab_mem = malloc(sizeof(cell_bell *) * nb_par);
+    E_glob = malloc(sizeof(long long **) * nb_par);
+    int **tab_ind = getIndiceLastChar(mmappedData, nb_par, filesize); //On recupere les indices de debut et de fin de
+    //chaque paragraphe
+    for (int i = 0; i < nb_par; ++i) {
+        text[i].ind_prem_car = tab_ind[0][i];
+        text[i].ind_dern_car = tab_ind[1][i];
+        text[i].nb_mots = recup_nb_mots(mmappedData, text[i].ind_prem_car, text[i].ind_dern_car);
+        text[i].tailles_mots = recup_tailles_mots(mmappedData, text
+                                                  [i].ind_prem_car,
+                                                  text[i].ind_dern_car, text
+                                                  [i].nb_mots);
+        text[i].mots = recup_mots(mmappedData, text[i].nb_mots, text[i].tailles_mots, text[i].ind_prem_car);
 
+        int *tab_calculs = calculs_paragraphes(i, text[i].mots, text[i].tailles_mots, text[i].nb_mots, M);
+        text[i].nb_espaces = tab_calculs[1];
+        text[i].nb_lignes = tab_calculs[0];
+        text[i].cout = tab_calculs[2];
+        text[i].lignes = malloc(sizeof(char *) * text[i].nb_lignes);
+        for (int j = 0; j < text[i].nb_lignes - 1; ++j) {
+            text[i].lignes[j] = malloc(sizeof(char) * (M + 1));
+        }
+        printf("%i\n", text[i].cout);
+        justifier_paragraphe(i, text[i], text[i].mots, text[i].tailles_mots, text[i].nb_mots, M, text[i].nb_lignes);
+    }
+    long long nb_lignes_tot = 0;
+    long long nb_espaces_tot = 0;
+    long long cout_tot = 0;
+    for (int k = 0; k < nb_par; ++k) {
+        cout_tot += text[k].cout;
+        nb_lignes_tot += text[k].nb_lignes;
+        nb_espaces_tot += text[k].nb_espaces;
+    }
     const char *filepath = ecriture_out(argv[2]);
 
-    printf("%s", filepath);
-    int fdout = open(filepath, O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600); //open crée le fichier ?
 
-    if (fdout == -1)
-    {
+    int fdout = open(filepath, O_RDWR | O_CREAT | O_TRUNC, (mode_t) 0600); //open crée le fichier
+
+    if (fdout == -1) {
         perror("Error opening file for writing");
         exit(EXIT_FAILURE);
     }
-
-    size_t sizeout = filesize; /// + le nb d'espaces ajoutés;
-
-    if (lseek(fdout, sizeout-1, SEEK_SET) == -1)
-    {
+    size_t sizeout = filesize + (nb_lignes_tot-nb_par) + nb_espaces_tot ; /// + le nb d'espaces ajoutés;
+    if (lseek(fdout, sizeout - 1, SEEK_SET) == -1) {
         close(fdout);
         perror("Error calling lseek() to 'stretch' the file");
         exit(EXIT_FAILURE);
     }
-
-    if (write(fdout, "", 1) == -1)
-    {
+    if (write(fdout, "", 1) == -1) {
         close(fdout);
         perror("Error writing last byte of the file");
         exit(EXIT_FAILURE);
     }
-
-    char *map = (char*) mmap(0, sizeout, PROT_READ | PROT_WRITE, MAP_SHARED, fdout, 0);
-
-    if (map == MAP_FAILED)
-    {
+    char *map = (char *) mmap(0, sizeout, PROT_READ | PROT_WRITE, MAP_SHARED, fdout, 0);
+    if (map == MAP_FAILED) {
         close(fdout);
         perror("Error mmapping the file");
         exit(EXIT_FAILURE);
     }
 
-
-    for (size_t i = 0; i < sizeout; i++)
-    {
-        map[i] = (char) "f"; // remplir avec Bellman;
+    for (int l = 0; l < nb_par; ++l) {
+        write_line_in_file(map, sizeout, text[l].lignes, text[l].nb_lignes, M);
     }
-
     // Write it now to disk
-    if (msync(map, sizeout, MS_SYNC) == -1)
-    {
+    if (msync(map, sizeout, MS_SYNC) == -1) {
         perror("Could not sync the file to disk");
     }
 
     // Don't forget to free the mmapped memory
-    if (munmap(map, sizeout) == -1)
-    {
+    if (munmap(map, sizeout) == -1) {
         close(fdout);
         perror("Error un-mmapping the file");
         exit(EXIT_FAILURE);
@@ -193,5 +516,5 @@ int main(int argc, char **argv) {
     close(fd);
 
 
-    return 0 ;
+    return 0;
 }
--- src/AODjustify.h
+++ src/AODjustify.h
@@ -6,18 +6,144 @@
 #define TPAODJUSTIFY_AODJUSTIFY_H
 
 
+
+/**
+ * Structure de bellman
+ */
 typedef struct {
     long long bell; // Valeur du bellman(i)
     int ind; // L'indice du dernier mot de la ligne commençant par le mot i
 } cell_bell;
 
 typedef struct {
-    char* mots; //Tableau contenant tous les mots du paragraphe
+    char **mots; //Tableau contenant tous les mots du paragraphe
+    int *tailles_mots;//Tableau contenant toutes les tailles des mots du paragraphe
     int nb_mots;
-    int ind_prem_mot;
-    int ind_dern_mot;
+    int ind_prem_car; //indice du premier caractère du paragraphe
+    int ind_dern_car; //indice du dernier caractère du paragraphe
+    char **lignes; //Tableau contenant toutes les lignes du paragraphe
+    int nb_lignes;
+    int nb_espaces;
+    int cout;
 } paragraphe;
 
+/**
+ *
+ * @param filename
+ * @return taille du fichier filename
+ */
+size_t getFilesize(const char *filename);
+
+/**
+ * ecrit le nom du fichier.out
+ * @param nom_in
+ * @return
+ */
+char *ecriture_out(const char *nom_in);
+
+
+/**
+ * Recupere le nombre de paragraphes du fichier file
+ * @param file
+ * @param filesize
+ * @return nb_paragraphes
+ */
+int recup_nb_para(char *file, int filesize);
+
+/**
+ * Retourne deux tableaux d'indices de debut et de fin de chaque paragraphe du fichier d'entree
+ * @param mmappedData
+ * @param nb_paragraphes
+ * @param filesize
+ * @return tableaux
+ */
+int **getIndiceLastChar(char *mmappedData, int nb_paragraphes, size_t filesize);
+
+/**
+ * recupere le nombre de mots du paragraphe commençant par le caractère d'indice debut et finissant par le caractere
+ * d'indice fin
+ * @param file
+ * @param debut
+ * @param fin
+ * @return nb_mots
+ */
+int recup_nb_mots(char *file, int debut, int fin);
+
+/**
+ * Recupere le tableau des tailles des mots du paragraphe
+ * @param file
+ * @param debut
+ * @param fin
+ * @param nb_mots
+ * @return tailles
+ */
+int *recup_tailles_mots(char *file, int debut, int fin, int nb_mots);
+
+
+/**
+ * Recupere un tableau contenant tous les mots du paragraphe (Ex : mots[15] est le 16e mot du paragraphe)
+ * @param file
+ * @param nb_mots
+ * @param tailles
+ * @param debut
+ * @return mots
+ */
+char **recup_mots(char *file, int nb_mots, int *tailles, int debut);
+
+
+/**
+ * Algorithme recursif de l'equation de Bellman pour le paragraphe numero num_par du fichier. On raisonne sur
+ * les mots et non sur les caractères.
+ * @param num_par
+ * @param nb_mots
+ * @param i
+ * @param M
+ * @return Bellman(i)
+ */
+long long Bellman(int num_par, int nb_mots, int i, long long M);
+
+/**
+ * Ecrit dans ligne la ligne du paragraphe num_par commençant par le mot d'indice first et finissant par le mot
+ * d'indice last.
+ * @param num_par
+ * @param ligne
+ * @param mots
+ * @param tailles
+ * @param first
+ * @param last
+ * @param M
+ * @param derniere_ligne
+ */
+void ecrire_ligne(int num_par, char *ligne, char **mots, int *tailles, int first, int last, long long M,
+                  bool derniere_ligne);
+
+/**
+ * Retourne le nb de lignes justifiées ainsi que le nombre d'espaces à rajouter du paragraphes num_par
+ * et calcule aussi les E(i,k) ainsi que les valeurs de bellman(i) pour chaque mot.
+ * Ce programme permet egalement de stocker les indices de mots de fin de ligne grâce à la structure cell_bell.
+ * @param num_par
+ * @param mots
+ * @param tailles
+ * @param nb_mots
+ * @param M
+ * @return
+ */
+long long *calculs_paragraphes(int num_par, char **mots, int *tailles, int nb_mots, long long M);
+
+
+
+/**
+ * Justifie le paragraphe num_par. Appelle la fonction ecrire_ligne et remplit les lignes du paragraphe.
+ * @param num_par
+ * @param paragraphe
+ * @param mots
+ * @param tailles
+ * @param nb_mots
+ * @param M
+ * @param nb_ligne
+ */
+void justifier_paragraphe(int num_par, paragraphe paragraphe, char **mots, int *tailles, int nb_mots, long long M,
+                          int nb_ligne);
 
 
 #endif //TPAODJUSTIFY_AODJUSTIFY_H
